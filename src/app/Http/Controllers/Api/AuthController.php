6a01fee9c66db722511849f0a21fb96c6660de7b
Yo Kamada <bumpfuji10@gmail.com>
2022-12-24 12:45:25 +0900

    add:ログインの実装

--- src/app/Http/Controllers/Api/AuthController.php
+++ src/app/Http/Controllers/Api/AuthController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
-use Illuminate\Http\Request;
 use App\Models\User;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -25,7 +27,17 @@ class AuthController extends Controller
 
     public function login(LoginRequest $request)
     {
+        $credentials = $request->validated();
+        if (!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'メールアドレスもしくはパスワードが一致しません'
+            ], 422);
+        }
 
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
     }
 
     public function logout(Request $request)
