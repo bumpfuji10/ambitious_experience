9365167270b16725a1972dbca11cca799d2f9fbf
Yo Kamada <bumpfuji10@gmail.com>
2022-12-24 11:40:24 +0900

    add:認証コントローラを作成

--- src/app/Http/Controllers/Api/AuthController.php
+++ src/app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+        /** @var User $user */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
+    }
+
+    public function signup(SignupRequest $request)
+    {
+
+    }
+
+    public function logout(Request $request)
+    {
+
+    }
 }
